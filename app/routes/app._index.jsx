6b698d885d0fe2bc7ae8ae9f63ace132708583f2
Nako <ff@gg.hh>
2023-09-08 01:24:38 +0500

    task 2 completed

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { json } from "@remix-run/node";
-import { useLoaderData, useSubmit } from "@remix-run/react";
+import {  useLoaderData, useSubmit } from "@remix-run/react";
 import {
   Page,
   Layout,
@@ -9,22 +9,44 @@ import {
   Card,
   Button,
   Box,
+  Tabs,
+  LegacyCard,
+  Key,
 } from "@shopify/polaris";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  const { session  } = await authenticate.admin(request);
+  // const { session  } = await authenticate.admin(request);
 
+  const {admin, session} = await authenticate.admin(request);
+  const data = await admin.rest.resources.Product.count({session});
+  
+ const theme = await admin.rest.resources.Theme.all({
+    session: session,
+    role:"main"
+  });
+  const theme_id = theme?.data[0]?.id;
+  const assets = await admin.rest.resources.Asset.all({
+    session: session,
+    theme_id: theme_id,
+  });
+
+  const home_templates = assets.data.filter(item => /^templates\/index.*\.json$/.test(item.key));
+  const product_templates = assets.data.filter(item => /^templates\/product.*\.json$/.test(item.key));
+  const collection_templates = assets.data.filter(item => /^templates\/collection.*\.json$/.test(item.key));
+
+// console.log(data);
   const shop = session.shop
   const apiAccessToken = session.accessToken;
   // TODO: Complete this function to fetch the assets from the Shopify API
   // and return the home, product, and collection templates.
 
-  return json({ data: {} });
+
+  return json({ data: {home:home_templates,product:product_templates,collection:collection_templates} });
 };
 
 export let action = async ({request}) => {
-  const { session  } = await authenticate.admin(request);
+  const { admin,session  } = await authenticate.admin(request);
 
   const shop = session.shop
   const apiAccessToken = session.accessToken;
@@ -32,7 +54,29 @@ export let action = async ({request}) => {
   // by creating a new asset with a random key and the same content.
   // format should be if homepage then index.{random10-characters-key}.liquid, collection then collection.{random10-characters-key}.liquid, product then product.{random10-characters-key}.liquid
 
-  return json({status: 'success'});
+  const form = await request.formData();
+  const key = form.get('key');
+  const theme_id = form.get('theme_id');
+
+  const time_rand = Date.now();
+
+  let match =  key.match(/\/(index|product|collection)\./);
+  match =  match? match[1]:"index";
+  let success = false;
+
+  try{
+  const asset = new admin.rest.resources.Asset({session: session});
+  asset.theme_id = theme_id;
+  asset.key = "templates/"+match+"."+time_rand+".json";
+  asset.source_key = key;
+  const data = await asset.save({
+    update: true,
+  });
+  success = true;
+}catch(error){
+  success = false;
+}
+  return json({success});
 };
 
 export default function Index() {
@@ -41,19 +85,44 @@ export default function Index() {
   const [selectedAsset, setSelectedAsset] = useState(null);
   const submit = useSubmit();
 
+
   const handleSelect = (asset) => {
     setSelectedAsset(asset);
   };
 
-  const handleDuplicate = () => {
+  const handleDuplicate = async () => {
     // TODO: Complete this function to submit the form with the selected asset key and theme ID.
+    
+    submit(selectedAsset, { replace: true, method: "POST" });
+ 
   };
 
-  const renderCard = (asset) => {
+  const RenderCard = ({asset}) => {
     // TODO: Complete this function to render a card for each asset with its key, theme ID, and updated at time.
+   
+    return (
+    <>
+        {asset.map((item, index) => (
+          <div key={index} style={{cursor:"pointer",border:selectedAsset?.key == item.key?"1px solid":"0px"}} onClick={()=>{setSelectedAsset(item)}} >
+          <LegacyCard.Section >
+            <p>Key: {item.key}</p>
+            <p>Updated At: {item.updated_at}</p>
+            <p>Theme ID: {item.theme_id}</p>
+          </LegacyCard.Section>
+          </div>
+        ))}
+      </>
+    );
   };
 
   // TODO: Create the Tabs and Panels components and render the assets inside the Panels.
+
+  const tabs = Object.keys(data).map((content,index) => ({ content:content+" Pages",id:content }));
+
+
+
+
+
 
   return (
     <Page>
@@ -62,6 +131,27 @@ export default function Index() {
         <Layout>
           <Layout.Section>
             {/* TODO: Render the Tabs and Panels components here */}
+
+            <LegacyCard>
+            <Tabs tabs={tabs} selected={selectedTabIndex} onSelect={(state)=>{setSelectedTabIndex(state)}}>
+        
+            {(() => {
+
+            switch (selectedTabIndex) {
+              case 0:
+                return <RenderCard  asset={data.home} />
+                break;
+              case 1:
+                return <RenderCard  asset={data.product} />
+                break;
+              case 2:
+                return <RenderCard  asset={data.collection} />
+                break;   
+            
+            }})()}
+      
+            </Tabs>
+          </LegacyCard>
           </Layout.Section>
         </Layout>
         <form method="post">
--- app/shopify.server.js
+++ app/shopify.server.js
@@ -3,17 +3,17 @@ import {
   AppDistribution,
   DeliveryMethod,
   shopifyApp,
-  LATEST_API_VERSION,
+  ApiVersion,
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-import { restResources } from "@shopify/shopify-api/rest/admin/2023-07";
+import { restResources } from "@shopify/shopify-api/rest/admin/2023-01";
 
 import prisma from "./db.server";
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  apiVersion: LATEST_API_VERSION,
+  apiVersion: ApiVersion.January23,
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
@@ -37,7 +37,7 @@ const shopify = shopifyApp({
 });
 
 export default shopify;
-export const apiVersion = LATEST_API_VERSION;
+export const apiVersion = ApiVersion.January23;
 export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
 export const authenticate = shopify.authenticate;
 export const unauthenticated = shopify.unauthenticated;
